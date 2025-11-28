0c03779dbf3864b8ad6c577d0226b444f689fa3f
huang-yi-dae <2014596548@qq.com>
2025-11-28 17:30:49 +0800

    Lab2 Finished

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,19 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimeNoPrime(){
+        IntList lst = IntList.of(1,6,8,9,12);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 6 -> 8 -> 9 -> 12", lst.toString());
+        assertFalse(changed);
+    }
+    @Test
+    public void testSquarePrimeAllPrime(){
+        IntList lst = IntList.of(2,3,7,13);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 49 -> 169", lst.toString());
+        assertTrue(changed);
+    }
 }
